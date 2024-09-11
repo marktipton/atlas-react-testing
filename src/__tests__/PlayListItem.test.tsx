1763967d8d042f225d82f43d446fad857f5bb4b2
marktipton <marktipton95@gmail.com>
2024-09-11 14:11:16 -0500

    playlistItem test with props provided

--- src/__tests__/PlayListItem.test.tsx
+++ src/__tests__/PlayListItem.test.tsx
@@ -3,6 +3,14 @@ import { expect, test } from "vitest";
 import  PlayListItem  from "../components/PlayListItem";
 
 test("volume control renders correctly w/", () => {
-  const { container } = render(<PlayListItem />);
+  const { container } = render(
+  <PlayListItem
+    name="Daily Routine"
+    artist="Animal Collective"
+    time="5:47"
+    bgColor="bg-secondary-light"
+    onClick={() => {}}
+  />
+  );
   expect(container).toMatchSnapshot();
 });
